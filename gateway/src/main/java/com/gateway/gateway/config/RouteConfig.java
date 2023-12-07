4f2feadc9581595801197fc609c2d5f7128a5207
Anastasia Malkova <ad.malkova73@gmail.com>
2023-12-07 12:30:50 +0400

    fixes

--- gateway/src/main/java/com/gateway/gateway/config/RouteConfig.java
+++ gateway/src/main/java/com/gateway/gateway/config/RouteConfig.java
@@ -1,5 +1,7 @@
 package com.gateway.gateway.config;
 
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,19 +11,25 @@ import org.springframework.http.HttpMethod;
 @Configuration
 public class RouteConfig {
 
+    @NotBlank
+    @Value("${routing.counterpart-url}")
+    private String counterpartUrl;
+
+    @NotBlank
+    @Value("${routing.report-url}")
+    private String reportUrl;
+
     @Bean
     RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("counterpart",
                         route -> route.path("/api/**")
-                                .filters(filter -> filter.stripPrefix(1)
-                                )
-                                .uri("http://localhost:8080/"))
+                                .filters(filter -> filter.stripPrefix(1))
+                                .uri(counterpartUrl))
                 .route("reports",
                         route -> route.path("/rep/**")
-                                .filters(filter -> filter.stripPrefix(1)
-                                )
-                                .uri("http://localhost:8084/"))
+                                .filters(filter -> filter.stripPrefix(1))
+                                .uri(reportUrl))
                 .build();
     }
 }
